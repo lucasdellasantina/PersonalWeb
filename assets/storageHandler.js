e53024e20d0e1726b25cb642cbe8848fda5d4322
Lucas Della Santina <lucasdellasantina.96@gmail.com>
2025-05-11 22:39:49 -0300

    test

--- assets/storageHandler.js
+++ assets/storageHandler.js
@@ -1,28 +1,12 @@
 (function() {
-    var isConsoleOpen = false;
-
-    // Función para detectar la apertura de la consola
-    var detectConsole = function() {
-        var devtools = /./;
-        devtools.toString = function() {
-            isConsoleOpen = true;
-            console.clear();  // Limpiar la consola para evitar cualquier acumulación
-            console.log("¡Detente!");
-            console.log("Esta función del navegador está pensada para desarrolladores.");
-            console.log("Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función de Facebook o para \"hackear\" la cuenta de alguien, se trata de un fraude.");
-            console.log("Si lo haces, esta persona podrá acceder a tu cuenta.");
-            console.log("Consulta https://www.facebook.com/selfxss para obtener más información.");
-        };
-
-        // Probar el método para detectar si la consola está abierta
-        console.log(devtools);
+    var devtools = /./;
+    devtools.toString = function() {
+        console.log("¡Detente!");
+        console.log("Esta función del navegador está pensada para desarrolladores.");
+        console.log("Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función de Facebook o para 'hackear' la cuenta de alguien, se trata de un fraude.");
+        console.log("Si lo haces, esta persona podrá acceder a tu cuenta.");
+        console.log("Consulta https://www.facebook.com/selfxss para obtener más información.");
     };
 
-    // Revisa cada segundo si la consola está abierta
-    setInterval(function() {
-        if (isConsoleOpen) {
-            return;
-        }
-        detectConsole();
-    }, 1000);
+    console.log(devtools);
 })();
