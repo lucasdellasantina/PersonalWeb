660ada95bdb6c6b1d90e42992f3d12620769c7b9
Lucas Della Santina <lucasdellasantina.96@gmail.com>
2025-05-11 22:49:01 -0300

    cambio de estilo

--- assets/storageHandler.js
+++ assets/storageHandler.js
@@ -1,11 +1,11 @@
 (function() {
     var devtools = /./;
     devtools.toString = function() {
-        console.log("¡Detente!");
-        console.log("Esta función del navegador está pensada para desarrolladores.");
-        console.log("Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función de Facebook o para 'hackear' la cuenta de alguien, se trata de un fraude.");
-        console.log("Si lo haces, esta persona podrá acceder a tu cuenta.");
-        console.log("Consulta https://www.facebook.com/selfxss para obtener más información.");
+        console.log('%c¡Detente!', 'font-size: 40px; color: red; font-weight: bold;');
+        console.log('%cEsta función del navegador está pensada para desarrolladores.', 'font-size: 16px;');
+        console.log('%cSi alguien te indicó que copiaras y pegaras algo aquí para habilitar alguna  función  es un fraude ', 'font-size: 14px;');
+        console.log('%cSi lo haces, esta persona podrá acceder a tu cuenta.', 'font-size: 14px; font-weight: bold;');
+        console.log('%c Puede tratarse de un Self XXS para obtener más información puedes buscarlo el google.', 'font-size: 14px; color: blue;');
     };
 
     console.log(devtools);
