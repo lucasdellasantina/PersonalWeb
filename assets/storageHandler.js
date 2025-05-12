ae1de535a06151b43cc96f8b8529f643edb21ae3
Lucas Della Santina <lucasdellasantina.96@gmail.com>
2025-05-11 22:32:05 -0300

    test

--- assets/storageHandler.js
+++ assets/storageHandler.js
@@ -1,19 +1,28 @@
 (function() {
-    const detectConsoleOpen = () => {
-        const start = new Date();
-        debugger;  // Usa el debugger para intentar detectar si la consola está abierta
-        const end = new Date();
-        
-        if (end - start > 100) {  // Si el tiempo entre estas dos líneas es mayor a un valor específico, es probable que la consola esté abierta
-            console.clear();  // Limpiar la consola para que no se acumule
+    var isConsoleOpen = false;
+
+    // Función para detectar la apertura de la consola
+    var detectConsole = function() {
+        var devtools = /./;
+        devtools.toString = function() {
+            isConsoleOpen = true;
+            console.clear();  // Limpiar la consola para evitar cualquier acumulación
             console.log("¡Detente!");
             console.log("Esta función del navegador está pensada para desarrolladores.");
             console.log("Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función de Facebook o para \"hackear\" la cuenta de alguien, se trata de un fraude.");
             console.log("Si lo haces, esta persona podrá acceder a tu cuenta.");
             console.log("Consulta https://www.facebook.com/selfxss para obtener más información.");
-        }
+        };
+
+        // Probar el método para detectar si la consola está abierta
+        console.log(devtools);
     };
 
-    // Llama a la función para detectar la consola abierta
-    setInterval(detectConsoleOpen, 1000);  // Revisa cada segundo
+    // Revisa cada segundo si la consola está abierta
+    setInterval(function() {
+        if (isConsoleOpen) {
+            return;
+        }
+        detectConsole();
+    }, 1000);
 })();
