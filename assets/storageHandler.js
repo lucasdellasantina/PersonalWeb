79a40057dd3551f74a7a867089996c3cf772c267
Lucas Della Santina <lucasdellasantina.96@gmail.com>
2025-05-11 23:01:00 -0300

    style

--- assets/storageHandler.js
+++ assets/storageHandler.js
@@ -2,10 +2,8 @@
     var devtools = /./;
     devtools.toString = function() {
         console.log('%c¡Detente!', 'font-size: 40px; color: red; font-weight: bold;');
-        console.log('%cEsta función del navegador está pensada para desarrolladores.', 'font-size: 20px;');
-        console.log('%cSi alguien te indicó que copiaras y pegaras algo aquí  es un fraude. Si lo haces, esta persona podrá acceder a tu cuenta. ', 'font-size: 20px; font-weight: bold;' );
+        console.log('%cEsta función del navegador está pensada para desarrolladores.  /n Si alguien te indicó que copiaras y pegaras algo aquí  es un fraude. Si lo haces, esta persona podrá acceder a tu cuenta. ', 'font-size: 20px; font-weight: bold;' );
         console.log('%c Puede tratarse de un Self XXS',  'font-size: 18px;color: red;font-weight: bold;');
     };
-
     console.log(devtools);
 })();
