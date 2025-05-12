5ca25e6a168496c34f7047448bf928a54f6146f1
Lucas Della Santina <lucasdellasantina.96@gmail.com>
2025-05-11 22:58:19 -0300

    cambio de stile

--- assets/storageHandler.js
+++ assets/storageHandler.js
@@ -2,10 +2,9 @@
     var devtools = /./;
     devtools.toString = function() {
         console.log('%c¡Detente!', 'font-size: 40px; color: red; font-weight: bold;');
-        console.log('%cEsta función del navegador está pensada para desarrolladores.', 'font-size: 16px;');
-        console.log('%cSi alguien te indicó que copiaras y pegaras algo aquí para habilitar alguna  función  es un fraude ', 'font-size: 14px;');
-        console.log('%cSi lo haces, esta persona podrá acceder a tu cuenta.', 'font-size: 14px; font-weight: bold;');
-        console.log('%c Puede tratarse de un Self XXS para obtener más información puedes buscarlo el google.', 'font-size: 14px; color: blue;');
+        console.log('%cEsta función del navegador está pensada para desarrolladores.', 'font-size: 20px;');
+        console.log('%cSi alguien te indicó que copiaras y pegaras algo aquí  es un fraude. Si lo haces, esta persona podrá acceder a tu cuenta. ', 'font-size: 20px; font-weight: bold;' );
+        console.log('%c Puede tratarse de un Self XXS',  'font-size: 18px;color: red;font-weight: bold;');
     };
 
     console.log(devtools);
